b2205b5a9311bf26cf69f994b002bfe0ad53bae8
Calvin K <calvinknaub@gmail.com>
2024-01-04 14:01:38 -0800

    finsihed all middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -23,7 +23,7 @@ async function checkUsernameFree(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
     if (!users.length) {
-      next()
+      next();
     } else {
       next({ status: 422, message: "Username taken" });
     }
@@ -40,8 +40,17 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next();
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (users.length) {
+      next();
+    } else {
+      next({ status: 401, message: "Invalid credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -53,7 +62,11 @@ function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next();
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ status: 422, message: "Password must be longer than 3 chars" });
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
